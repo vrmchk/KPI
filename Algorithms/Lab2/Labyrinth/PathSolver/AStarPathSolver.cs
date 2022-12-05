71ed03606c43b4b86f1bf1c334d00ffbbc3e5847
vrmchk <ihorveremchuk8@gmail.com>
2022-12-05 17:21:29 +0200

    Lab2 done

---
+++ Algorithms/Lab2/Labyrinth/PathSolver/AStarPathSolver.cs
@@ -0,0 +1,47 @@
+﻿using Labyrinth.Common;
+using Labyrinth.Model;
+using Labyrinth.Utils;
+
+namespace Labyrinth.PathSolver;
+
+internal class AStarPathSolver : IPathSolver
+{
+    private int _iteration = 0;
+
+    public SearchResult Solve(State state, bool printSteps = false)
+    {
+        _iteration = 0;
+        return AStar(state, printSteps);
+    }
+
+    private SearchResult AStar(State state, bool printSteps)
+    {
+        var open = new HashSet<State>();
+        var closed = new HashSet<State>();
+        open.Add(state);
+        while (open.Count != 0)
+        {
+            var current = open.MinBy(s => s.Evaluation)!;
+            open.Remove(current);
+            closed.Add(current);
+            
+            _iteration++;
+            if (printSteps)
+                current.PrintState();
+            
+            if (current.Distance == 1)
+                return new SearchResult(current, _iteration, open.Count + closed.Count);
+            
+            foreach (var child in current.GetNeighbors())
+            {
+                if (closed.Contains(child))
+                    continue;
+                
+                if (!open.Contains(child))
+                    open.Add(child);
+            }
+        }
+
+        return new SearchResult(null, int.MaxValue, int.MaxValue);
+    }
+}
---
+++ Algorithms/Lab2/Labyrinth/PathSolver/BfsPathSolver.cs
@@ -0,0 +1,49 @@
+﻿using Labyrinth.Common;
+using Labyrinth.Model;
+using Labyrinth.Utils;
+
+namespace Labyrinth.PathSolver;
+internal class BfsPathSolver : IPathSolver
+{
+    private int _iteration = 0;
+    
+    public SearchResult Solve(State state, bool printSteps = false)
+    {
+        int states = state.GetDepth().Count;
+        return Bfs(state, states, printSteps);
+    }
+
+    private SearchResult Bfs(State state, int depth, bool printSteps)
+    {
+        var visited = new HashSet<State>();
+        var queue = new Queue<State>();
+        queue.Enqueue(state);
+        while (queue.Count != 0)
+        {
+            var current = queue.Dequeue();
+            
+            if (visited.Contains(current))
+                continue;
+
+            visited.Add(current);
+            
+            if (current.Generation >= depth) 
+                return new SearchResult(null, int.MaxValue, int.MaxValue);
+
+            _iteration++;
+            if (printSteps)
+                current.PrintState();
+
+            if (current.Distance == 1)
+                return new SearchResult(current, _iteration, queue.Count + current.Generation);
+
+            foreach (var child in current.GetNeighbors())
+            {
+                if (!visited.Contains(child))
+                    queue.Enqueue(child);
+            }
+        }
+        
+        return new SearchResult(null, int.MaxValue ,int.MaxValue);
+    }
+}
