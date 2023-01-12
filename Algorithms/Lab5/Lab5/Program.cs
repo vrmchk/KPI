33ff9c8ba27acf407b68e621b4b6992ea520844f
vrmchk <ihorveremchuk8@gmail.com>
2023-01-12 22:24:14 +0200

    Lab5 added

---
+++ Algorithms/Lab5/Lab5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using Lab5;
+
+var problem  = new Problem();
+var result = new StringBuilder();
+var batchNumber = 4;
+var probability = 0.3;
+var algorithm = new GeneticAlgorithm(batchNumber, probability, problem);
+for (int i = 0; i < 500; i++)
+{
+    for (int j = 0; j < 20; j++)
+    {
+        algorithm.Iterate();
+    }
+
+    var line = $"Iteration: {i * 20 + 20}, Cost: {algorithm.GetBestSolution().Cost}";
+    result.AppendLine(line);
+    Console.WriteLine(line);
+}
+
+File.WriteAllText("result.txt", result.ToString());
